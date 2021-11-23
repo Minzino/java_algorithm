c758f36aeba639f515e074f4f3a1104181ddf5e9
Minzino <gurumcider@gmail.com>
2021-11-23 11:25:39 +0900

    백준 commit

---
+++ src/baekjoon/bj2438.java
@@ -0,0 +1,22 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class bj2438 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int number = Integer.parseInt(br.readLine()); //숫자입력
+		br.close();		
+		StringBuilder sb = new StringBuilder();		
+		for(int i = 0; i < number; i++) {
+			for(int j = 0; j <= i; j++) {
+				sb.append("*");
+			}
+			sb.append("\n");
+		}		
+		System.out.println(sb); //출력부분
+	}
+
+}
---
+++ src/baekjoon/bj8958.java
@@ -0,0 +1,30 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class bj8958 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int test_case = Integer.parseInt(br.readLine()); //테스트 케이스입력
+		String score[] = new String[test_case];
+
+		for (int i = 0; i < test_case; i++) {
+			int sum = 0;
+			int count = 0;
+			score[i] = br.readLine();
+			for(int j = 0; j < score[i].length(); j++) {
+
+				if(score[i].charAt(j) == 'O') {
+					count++;
+					sum += count;
+				}else if(score[i].charAt(j) == 'X') {
+					count = 0;
+				}
+			}
+			System.out.println(sum);
+		}
+	}
+
+}

4489ca44553c5d51d5db30c2a608d8e1ec9f712a
Minzino <gurumcider@gmail.com>
2021-11-26 14:56:24 +0900

    Update 백준 while문

--- baekjoon/while_loop/bj10951.java
+++ baekjoon/while_loop/bj10951.java
@@ -13,9 +13,8 @@ public class bj10951 {
 		String str;
 		while ((str = br.readLine()) != null) {
 			/**
-			 * readLine() 을 통해 입력을 하여 
-			 * str 에 저장된 데이터가 null 일 경우 
-			 * while 반복문을 종료시켜버리고 아닐경우 반복문을 계속 수행하도록 한다.
+			 * readLine() 을 통해 입력을 하여 str 에 저장된 데이터가 null 일 경우 while 반복문을 종료시켜버리고 아닐경우 반복문을
+			 * 계속 수행하도록 한다.
 			 */
 			st = new StringTokenizer(str, " ");
 			int first = Integer.parseInt(st.nextToken());

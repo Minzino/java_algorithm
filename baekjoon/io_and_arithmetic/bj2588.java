2513bd54d00373da92632caeac852ef712247274
Minzino <gurumcider@gmail.com>
2021-12-26 18:16:24 +0900

    Update bj2588

--- baekjoon/io_and_arithmetic/bj2588.java
+++ baekjoon/io_and_arithmetic/bj2588.java
@@ -4,23 +4,15 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class bj2588 {
-
-	public static void main(String[] args) throws IOException {
+class bj2588{
+	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
 		int first = Integer.parseInt(br.readLine());
-		String[] second = br.readLine().split("");
-		int[] result = new int[second.length];
-		int j = second.length;
-
-		for (int i = 0; i < second.length; i++) {
-			result[i] = first * Integer.parseInt(second[j-1]);
-			j--;
-		}
-		System.out.println(result[0]);
-		System.out.println(result[1]);
-		System.out.println(result[2]);
-		System.out.println(result[0] + (result[1] * 10) + (result[2] * 100));
+		String second = br.readLine();
+		System.out.println(first*(second.charAt(2)-'0'));
+		System.out.println(first*(second.charAt(1)-'0'));
+		System.out.println(first*(second.charAt(0)-'0'));
+		System.out.println(first*Integer.parseInt(second));
 	}
-
 }

c97ccbe57106d15c503e2fc5524d5eb22f814c55
Meenzino <86910955+Minzino@users.noreply.github.com>
2022-01-26 23:30:21 +0900

    Update bj2309 다른풀이 추가

--- baekjoon/array/bj2309.java
+++ baekjoon/array/bj2309.java
@@ -23,7 +23,7 @@ public class bj2309 {
 
 
 
-        int temp1=0;
+        int temp1 = 0;
         for(int i = 0 ; i <9; i++){
             if(temp1 == 100) break;
             temp1 = sum;
@@ -47,3 +47,47 @@ public class bj2309 {
         System.out.println(sb);
     }
 }
+//다른 풀이
+//package array;
+//
+//        import java.io.BufferedReader;
+//        import java.io.IOException;
+//        import java.io.InputStreamReader;
+//        import java.util.Arrays;
+//
+//public class bj2309 {
+//    public static void main(String[] args) throws IOException {
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        StringBuilder sb = new StringBuilder();
+//
+//
+//        int sum = 0;
+//        int[] arrDwarf = new int[9];
+//
+//        for (int i = 0; i < 9; i++) {
+//            arrDwarf[i] = Integer.parseInt(br.readLine());
+//            sum += arrDwarf[i];
+//        }
+//        Arrays.sort(arrDwarf);
+//
+//
+//        boolean flag = false;
+//        for (int i = 0; i < 9; i++) {
+//            for (int j = i; j < 9; j++) {
+//                if (arrDwarf[i] + arrDwarf[j] == sum - 100) {
+//                    arrDwarf[i] = arrDwarf[j] = 0;
+//                    flag = true;
+//                    break;
+//                }
+//            }
+//            if (flag) break;
+//        }
+//
+//        for (int i = 0; i < 9; i++) {
+//            if (arrDwarf[i] != 0) {
+//                sb.append(arrDwarf[i]).append("\n");
+//            }
+//        }
+//        System.out.println(sb);
+//    }
+//}
